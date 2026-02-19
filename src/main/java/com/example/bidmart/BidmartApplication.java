dff22d94d1e67ded40ef2a43505a2bd343d3bf6f
kyraa16 <kadekchandra16@gmail.com>
2026-02-19 18:29:20 +0700

    Initial commit

---
+++ src/main/java/com/example/bidmart/BidmartApplication.java
@@ -0,0 +1,13 @@
+package com.example.bidmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidmartApplication.class, args);
+	}
+
+}
