b44305dec1923191a5cc89cc71af741f5830f582
karlaraswanda <karla.raswanda@gmail.com>
2026-03-05 13:54:24 +0700

    fix: implement Category entity

--- src/main/java/com/example/bidmart/listing/model/Category.java
+++ src/main/java/com/example/bidmart/listing/model/Category.java
@@ -1,4 +1,34 @@
 package com.example.bidmart.listing.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Column;
+
+import java.util.UUID;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name = "categories")
 public class Category {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(name = "parent_id")
+    private UUID parentId;
 }
