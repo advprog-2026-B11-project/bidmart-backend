02165928c362930921ac5dce565fdfb0261d3a79
kyraa16 <kadekchandra16@gmail.com>
2026-03-05 13:06:59 +0700

    feat: create Bid model

--- src/main/java/com/example/bidmart/bidding/model/Bid.java
+++ src/main/java/com/example/bidmart/bidding/model/Bid.java
@@ -1,4 +1,38 @@
 package com.example.bidmart.bidding.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+
+import java.util.UUID;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "bids")
+
 public class Bid {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private UUID listingId;
+    private UUID buyerId;
+    private BigDecimal amount;
+    private Boolean isProxyBid;
+    private BigDecimal proxyMaxLimit;
+    private LocalDateTime createdAt;
 }
+
+
