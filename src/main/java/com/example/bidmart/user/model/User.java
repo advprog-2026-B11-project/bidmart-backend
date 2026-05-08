ef42fd7582179f639a164796831a7ff0b35a21d6
HODEMASTER <peteryap0505@gmail.com>
2026-05-08 15:27:10 +0700

    refactor: changing role from enum to relational and adding permission model

--- src/main/java/com/example/bidmart/user/model/User.java
+++ src/main/java/com/example/bidmart/user/model/User.java
@@ -30,8 +30,8 @@ public class User {
     @Column(nullable = false)
     private String password;
 
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "role_id", nullable = false)
     private Role role;
 
     @Column(name = "is_active", nullable = false, columnDefinition = "boolean default true")
